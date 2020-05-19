d62a3ba1dc8cc5d28c083cd1ebdd937676f6afa2
songzxDev <songzx_2326@163.com>
2020-05-19 16:25:13 +0800

    电话号字母组合

--- mytest/week01/leetcode01.go
+++ mytest/week01/leetcode01.go
@@ -1,6 +1,7 @@
-package week01
+package main
 
 import (
+	"fmt"
 	"sort"
 )
 
@@ -353,7 +354,6 @@ func preorderTraversal(root *TreeNode) []int {
 //
 // Related Topics 链表
 
-
 //leetcode submit region begin(Prohibit modification and deletion)
 /**
  * Definition for singly-linked list.
@@ -363,9 +363,10 @@ func preorderTraversal(root *TreeNode) []int {
  * }
  */
 type ListNode struct {
-	Val int
+	Val  int
 	Next *ListNode
 }
+
 func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
 	if l1 == nil {
 		return l2
@@ -379,6 +380,7 @@ func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
 		return l2
 	}
 }
+
 //leetcode submit region end(Prohibit modification and deletion)
 
 //反转一个单链表。
@@ -391,7 +393,6 @@ func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
 // 进阶:
 //你可以迭代或递归地反转链表。你能否用两种方法解决这道题？
 // Related Topics 链表
-
 
 //leetcode submit region begin(Prohibit modification and deletion)
 /**
@@ -410,4 +411,48 @@ func reverseList(head *ListNode) *ListNode {
 	head.Next = nil
 	return after
 }
+
 //leetcode submit region end(Prohibit modification and deletion)
+
+//给定一个仅包含数字 2-9 的字符串，返回所有它能表示的字母组合。
+//
+// 给出数字到字母的映射如下（与电话按键相同）。注意 1 不对应任何字母。
+//
+//
+//
+// 示例:
+//
+// 输入："23"
+//输出：["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+//
+//
+// 说明:
+//尽管上面的答案是按字典序排列的，但是你可以任意选择答案输出的顺序。
+// Related Topics 字符串 回溯算法
+
+//leetcode submit region begin(Prohibit modification and deletion)
+func letterCombinations(digits string) []string {
+	if len(digits) == 0 {
+		return []string{}
+	}
+	tels := []string{"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"}
+	var letterHelper func(digits string, res *[]string, s string, depth int) []string
+	letterHelper = func(digits string, res *[]string, s string, depth int) []string {
+		if len(s) == len(digits) {
+			*res = append(*res, s)
+			return *res
+		}
+		coolie := tels[digits[depth] - '0']
+		for _,c := range coolie {
+			letterHelper(digits, res, s + string(c), depth + 1)
+		}
+		return *res
+	}
+	return letterHelper(digits, &[]string{}, "", 0)
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
+
+func main() {
+	fmt.Println(letterCombinations("5"))
+}
