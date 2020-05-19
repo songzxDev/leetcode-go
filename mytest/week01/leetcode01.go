9c02f0398c34c6d6f8a60831b7a1f505957747c5
songzxDev <songzx_2326@163.com>
2020-05-19 12:57:13 +0800

    二叉树的前序遍历

--- mytest/week01/leetcode01.go
+++ mytest/week01/leetcode01.go
@@ -301,3 +301,45 @@ func inorderTraversal(root *TreeNode) []int {
 }
 
 //leetcode submit region end(Prohibit modification and deletion)
+
+//给定一个二叉树，返回它的 前序 遍历。
+//
+// 示例:
+//
+// 输入: [1,null,2,3]
+//   1
+//    \
+//     2
+//    /
+//   3
+//
+//输出: [1,2,3]
+//
+//
+// 进阶: 递归算法很简单，你可以通过迭代算法完成吗？
+// Related Topics 栈 树
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func preorderTraversal(root *TreeNode) []int {
+	var treeHelper func(node *TreeNode, res *[]int) []int
+	treeHelper = func(node *TreeNode, res *[]int) []int {
+		if node == nil {
+			return *res
+		}
+		*res = append(*res, node.Val)
+		treeHelper(node.Left, res)
+		treeHelper(node.Right, res)
+		return *res
+	}
+	return treeHelper(root, &[]int{})
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
