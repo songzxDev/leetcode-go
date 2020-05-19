b3158e24d67aab8c40ddd89fdddf34af410193a6
songzxDev <songzx_2326@163.com>
2020-05-19 15:44:12 +0800

    合并两个有序链表

--- mytest/week01/leetcode01.go
+++ mytest/week01/leetcode01.go
@@ -343,3 +343,40 @@ func preorderTraversal(root *TreeNode) []int {
 }
 
 //leetcode submit region end(Prohibit modification and deletion)
+
+//将两个升序链表合并为一个新的升序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+//
+// 示例：
+//
+// 输入：1->2->4, 1->3->4
+//输出：1->1->2->3->4->4
+//
+// Related Topics 链表
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+type ListNode struct {
+	Val int
+	Next *ListNode
+}
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	if l1 == nil {
+		return l2
+	} else if l2 == nil {
+		return l1
+	} else if l1.Val < l2.Val {
+		l1.Next = mergeTwoLists(l1.Next, l2)
+		return l1
+	} else {
+		l2.Next = mergeTwoLists(l1, l2.Next)
+		return l2
+	}
+}
+//leetcode submit region end(Prohibit modification and deletion)
