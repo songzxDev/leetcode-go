499c73908d49a31e1982cda9c334558fde374700
songzxDev <songzx_2326@163.com>
2020-05-19 12:38:51 +0800

    有效的字母异位词分组

--- mytest/week01/leetcode01.go
+++ mytest/week01/leetcode01.go
@@ -214,3 +214,50 @@ func generateParenthesis(n int) []string {
 }
 
 //leetcode submit region end(Prohibit modification and deletion)
+
+//给定一个字符串数组，将字母异位词组合在一起。字母异位词指字母相同，但排列不同的字符串。
+//
+// 示例:
+//
+// 输入: ["eat", "tea", "tan", "ate", "nat", "bat"]
+//输出:
+//[
+//  ["ate","eat","tea"],
+//  ["nat","tan"],
+//  ["bat"]
+//]
+//
+// 说明：
+//
+//
+// 所有输入均为小写字母。
+// 不考虑答案输出的顺序。
+//
+// Related Topics 哈希表 字符串
+
+//leetcode submit region begin(Prohibit modification and deletion)
+func groupAnagrams(strs []string) [][]string {
+	var res [][]string
+	if strs == nil || len(strs) == 0 {
+		return res
+	}
+	primes := []int{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101}
+	getNumKey := func(stt string) int {
+		numKey := 1
+		for _, v := range stt {
+			numKey *= primes[v - 'a']
+		}
+		return numKey
+	}
+	countMap := make(map[int][]string)
+	for _, stt := range strs {
+		numKey := getNumKey(stt)
+		countMap[numKey] = append(countMap[numKey], stt)
+	}
+	for _, value := range countMap {
+		res = append(res, value)
+	}
+	return res
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
