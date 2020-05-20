dddefa1fa5ba47e0d539b9f0be1f31e9d72fd6f3
songzxDev <songzx_2326@163.com>
2020-05-20 12:36:52 +0800

    旋转数组

--- mytest/week01/leetcode01.go
+++ mytest/week01/leetcode01.go
@@ -787,6 +787,56 @@ func rob(nums []int) int {
 }
 
 //leetcode submit region end(Prohibit modification and deletion)
+//给定一个数组，将数组中的元素向右移动 k 个位置，其中 k 是非负数。
+//
+// 示例 1:
+//
+// 输入: [1,2,3,4,5,6,7] 和 k = 3
+//输出: [5,6,7,1,2,3,4]
+//解释:
+//向右旋转 1 步: [7,1,2,3,4,5,6]
+//向右旋转 2 步: [6,7,1,2,3,4,5]
+//向右旋转 3 步: [5,6,7,1,2,3,4]
+//
+//
+// 示例 2:
+//
+// 输入: [-1,-100,3,99] 和 k = 2
+//输出: [3,99,-1,-100]
+//解释:
+//向右旋转 1 步: [99,-1,-100,3]
+//向右旋转 2 步: [3,99,-1,-100]
+//
+// 说明:
+//
+//
+// 尽可能想出更多的解决方案，至少有三种不同的方法可以解决这个问题。
+// 要求使用空间复杂度为 O(1) 的 原地 算法。
+//
+// Related Topics 数组
+
+//leetcode submit region begin(Prohibit modification and deletion)
+func rotate(nums []int, k int) {
+	if nums == nil || len(nums) < 2 {
+		return
+	}
+	n := len(nums)
+	k %= n
+	if k > 0 {
+		rotateHelper := func(nums []int, begin int, end int) {
+			for begin < end {
+				nums[begin], nums[end] = nums[end], nums[begin]
+				begin++
+				end--
+			}
+		}
+		rotateHelper(nums, 0, n-1)
+		rotateHelper(nums, 0, k-1)
+		rotateHelper(nums, k, n-1)
+	}
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
 
 func main() {
 	/*node := &Node{
@@ -796,5 +846,7 @@ func main() {
 			&Node{Val: 3, Children: []*Node{}},
 		},
 	}*/
-	fmt.Println(rob([]int{2, 7, 9, 3, 1}))
+	testList := []int{1,2,3,4,5,6,7}
+	rotate(testList, 3)
+	fmt.Println(testList)
 }
