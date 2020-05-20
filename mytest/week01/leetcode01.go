a984d3b0edd58ea6a7779799be2959705def352e
songzxDev <songzx_2326@163.com>
2020-05-20 10:47:47 +0800

    N 叉树的后序遍历

--- mytest/week01/leetcode01.go
+++ mytest/week01/leetcode01.go
@@ -632,6 +632,53 @@ func levelOrder(root *Node) [][]int {
 }
 
 //leetcode submit region end(Prohibit modification and deletion)
+//给定一个 N 叉树，返回其节点值的后序遍历。
+//
+// 例如，给定一个 3叉树 :
+//
+//
+//
+//
+//
+//
+//
+// 返回其后序遍历: [5,6,3,2,4,1].
+//
+//
+//
+// 说明: 递归法很简单，你可以使用迭代法完成此题吗? Related Topics 树
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a Node.
+ * type Node struct {
+ *     Val int
+ *     Children []*Node
+ * }
+ */
+
+func postorder(root *Node) []int {
+	if root == nil {
+		return []int{}
+	}
+	var res []int
+	var helper func(node *Node)
+	helper = func(node *Node) {
+		if node == nil {
+			return
+		}
+		if node.Children != nil {
+			for _, val := range node.Children {
+				helper(val)
+			}
+		}
+		res = append(res, node.Val)
+	}
+	helper(root)
+	return res
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
 
 func main() {
 	node := &Node{
@@ -641,5 +688,5 @@ func main() {
 			&Node{Val: 3, Children: []*Node{}},
 		},
 	}
-	fmt.Println(levelOrder(node))
+	fmt.Println(postorder(node))
 }
