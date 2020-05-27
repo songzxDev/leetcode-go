4491e3847ba037a5788291461d4f4cc1474d4496
songzxDev <songzx_2326@163.com>
2020-05-27 08:29:21 +0800

    go return 终止main函数运行

--- mytest/week01/mysyncdemo.go
+++ mytest/week01/mysyncdemo.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	var i32 int32 = int32(97)
 	var o32 int32 = i32
-	for i:= 0; i < int(o32); i++ {
+	for i := 0; i < int(o32); i++ {
 		go func(k int) {
 			myi32 := atomic.AddInt32(&i32, -1)
 			fmt.Printf("i的值是：%d，myi32的值是：%d\n", k, myi32)
@@ -17,9 +17,23 @@ func main() {
 	}
 	for {
 		if atomic.LoadInt32(&i32) == 0 {
-			fmt.Println("The second number has gone to zero.")
-			break
+			fmt.Printf("myi32的值是：%d\n", i32)
+			func() {
+				for i := 0; i < 1; i++ {
+					fmt.Println("abc")
+					func() {
+						for j := 1; j < 100; j++ {
+							if j == 99 {
+								return
+							}
+						}
+					}()
+					return
+				}
+			}()
+			return
 		}
-		time.Sleep(time.Millisecond * 500)
+		time.Sleep(time.Millisecond * 1)
 	}
+	fmt.Println("Main exit.")
 }
