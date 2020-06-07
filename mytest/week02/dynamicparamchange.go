a2651c01be999413b1866a9ceaaa218812a07603
songzxDev <songzx_2326@163.com>
2020-06-07 17:00:20 +0800

    模拟携程发送请求

--- mytest/week02/dynamicparamchange.go
+++ mytest/week02/dynamicparamchange.go
@@ -1,57 +1,71 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"os"
+	"os/exec"
 	"sync/atomic"
 	"time"
 )
 
+func init() {
+	myCmd := exec.Command("echo", "''> D:\\tmp\\mydynamicparam.log")
+	myCmd.Stdout = os.Stdout
+	_ = myCmd.Run()
+	file := "D:\\tmp\\mydynamicparam.log"
+	logFile, err := os.OpenFile(file, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0766)
+	if err != nil {
+		panic(err)
+	}
+	log.SetOutput(logFile) // 将文件设置为log输出的文件
+	log.SetPrefix("[qSkipTool]")
+	log.SetFlags(log.LstdFlags | log.Lshortfile | log.LUTC)
+	return
+}
 func main() {
-
-	var myUrl string = "abc"
-	quitChan := make(chan bool)
-	bookChan := make(chan bool)
-	var pivot int32
+	bookChan, myPivot, myUrl, quitChan := make(chan bool), int32(0), "abc", make(chan bool)
+	mySleep := func(t time.Duration, doSomething func()) {
+		atomic.StoreInt32(&myPivot, -1)
+		doSomething()
+		time.Sleep(t)
+		atomic.StoreInt32(&myPivot, 1)
+		close(bookChan)
+		bookChan = make(chan bool)
+	}
 	go func() {
-		timer := time.NewTicker(50 * time.Second)
+		timer := time.NewTicker(51 * time.Second)
 		for {
 			select {
 			case <-timer.C:
-				atomic.StoreInt32(&pivot, -1)
-				myUrl = "bcd"
-				time.Sleep(10 * time.Second)
-				close(bookChan)
-				bookChan = make(chan bool)
-				atomic.StoreInt32(&pivot, 1)
-				time.Sleep(2 * time.Second)
-				atomic.StoreInt32(&pivot, -1)
-				time.Sleep(10 * time.Second)
-				myUrl = "abc"
-				close(bookChan)
-				bookChan = make(chan bool)
-				atomic.StoreInt32(&pivot, 1)
+				// 所有 goroutine 暂停运行10s
+				mySleep(10*time.Second, func() {
+					myUrl = "cba"
+				})
+				time.Sleep(1100 * time.Millisecond)
+				mySleep(10*time.Second, func() {
+					myUrl = "abc"
+				})
 			default:
 				continue
 			}
 		}
+
 	}()
-	for i := 0; i < 100; i++ {
-		go func() {
+
+	for i := 0; i < 64000; i++ {
+		go func(j int) {
 			for {
 				select {
 				case <-quitChan:
 					return
 				default:
-					if atomic.AddInt32(&pivot, 0) < 0 {
+					if atomic.AddInt32(&myPivot, 0) < 0 {
 						<-bookChan
 					}
-					fmt.Print(myUrl)
-
+					log.Printf("%s+%d", myUrl, j)
 				}
 			}
-
-		}()
+		}(i + 1)
 	}
-
 	time.Sleep(5 * time.Minute)
 }
