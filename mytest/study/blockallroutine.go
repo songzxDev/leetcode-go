88e015bf0aeb9f3b4c8617e744734d25df075d4b
songzxDev <songzx_2326@163.com>
2020-06-15 08:49:37 +0800

    调整携程阻塞逻辑

--- mytest/study/blockallroutine.go
+++ mytest/study/blockallroutine.go
@@ -6,34 +6,36 @@ import (
 	"time"
 )
 
+var (
+	threshold int32         = 1
+	bookChan  chan bool     = make(chan bool)
+	takeTime  time.Duration = 10 * time.Second
+)
+
 func main() {
-	threshold, bookChan := int32(0), make(chan bool)
 	for i := 0; i < 100; i++ {
 		go func(j int) {
 			for {
 				if atomic.LoadInt32(&threshold) < 0 {
-					fmt.Printf("携程【%d】阻塞中......\n", j)
+					fmt.Printf("goroutine【%d】阻塞中%v......\n", j, time.Now().UnixNano())
 					<-bookChan
 				} else {
-					fmt.Printf("携程【%d】运行中%v\n", j, time.Now().UnixNano())
+					fmt.Printf("goroutine【%d】运行中%v......\n", j, time.Now().UnixNano())
 				}
 			}
 		}(i)
 	}
 	go func() {
-		timer := time.NewTicker(10 * time.Second)
+		ticker := time.NewTicker(takeTime)
 		for {
 			select {
-			case <-timer.C:
+			case <-ticker.C:
 				atomic.StoreInt32(&threshold, -1)
-				time.Sleep(5 * time.Second)
+				time.Sleep(takeTime / 2)
 				atomic.StoreInt32(&threshold, 1)
 				close(bookChan)
 				bookChan = make(chan bool)
-			default:
-				continue
 			}
-
 		}
 	}()
 	time.Sleep(2 * time.Minute)
