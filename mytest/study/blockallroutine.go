3290c1442dff59501f9f6b934261dc0d9473a85c
songzxDev <songzx_2326@163.com>
2020-06-15 20:12:33 +0800

    携程阻塞逻辑

--- mytest/study/blockallroutine.go
+++ mytest/study/blockallroutine.go
@@ -9,36 +9,39 @@ import (
 var (
 	threshold int32         = 1
 	bookChan  chan bool     = make(chan bool)
-	takeTime  time.Duration = 10 * time.Second
+	workerNum int           = 100
+	roundTime time.Duration = 30 * time.Second
+	runTime   time.Duration = 4 * roundTime
 )
 
-func main() {
-	for i := 0; i < 100; i++ {
-		go func(j int) {
-			for {
-				if atomic.LoadInt32(&threshold) < 0 {
-					fmt.Printf("goroutine【%d】阻塞中%v......\n", j, time.Now().UnixNano())
-					<-bookChan
-				} else {
-					fmt.Printf("goroutine【%d】运行中%v......\n", j, time.Now().UnixNano())
-				}
-			}
-		}(i)
+func working(g int) {
+	for {
+		if atomic.LoadInt32(&threshold) < 0 {
+			fmt.Printf("goroutine【%d】 sleep at %v......\n", g, time.Now().UnixNano())
+			<-bookChan
+		} else {
+			fmt.Printf("goroutine【%d】 at %v running......\n", g, time.Now().UnixNano())
+		}
 	}
+}
+func main() {
 	go func() {
-		ticker := time.NewTicker(takeTime)
+		ticker := time.NewTicker(roundTime)
 		for {
 			select {
 			case <-ticker.C:
 				atomic.StoreInt32(&threshold, -1)
-				time.Sleep(takeTime / 2)
+				fmt.Println(roundTime/ 2)
+				time.Sleep(roundTime / 2)
 				atomic.StoreInt32(&threshold, 1)
 				close(bookChan)
 				bookChan = make(chan bool)
-			default:
-				continue
 			}
 		}
 	}()
-	time.Sleep(2 * time.Minute)
+	for i := 0; i < workerNum; i++ {
+		go working(i)
+	}
+
+	time.Sleep(runTime)
 }
