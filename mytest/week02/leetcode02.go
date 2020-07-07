1baa6789f209f31c603211719cb2f92443939d48
songzxDev <songzx_2326@163.com>
2020-07-07 08:21:43 +0800

    岛屿数量

--- mytest/week02/leetcode02.go
+++ mytest/week02/leetcode02.go
@@ -584,6 +584,66 @@ func sortArray(nums []int) []int {
 }
 
 //leetcode submit region end(Prohibit modification and deletion)
+//给你一个由 '1'（陆地）和 '0'（水）组成的的二维网格，请你计算网格中岛屿的数量。
+//
+// 岛屿总是被水包围，并且每座岛屿只能由水平方向或竖直方向上相邻的陆地连接形成。
+//
+// 此外，你可以假设该网格的四条边均被水包围。
+//
+//
+//
+// 示例 1:
+//
+// 输入:
+//[
+//['1','1','1','1','0'],
+//['1','1','0','1','0'],
+//['1','1','0','0','0'],
+//['0','0','0','0','0']
+//]
+//输出: 1
+//
+//
+// 示例 2:
+//
+// 输入:
+//[
+//['1','1','0','0','0'],
+//['1','1','0','0','0'],
+//['0','0','1','0','0'],
+//['0','0','0','1','1']
+//]
+//输出: 3
+//解释: 每座岛屿只能由水平和/或竖直方向上相邻的陆地连接而成。
+//
+// Related Topics 深度优先搜索 广度优先搜索 并查集
+
+//leetcode submit region begin(Prohibit modification and deletion)
+func numIslands(grid [][]byte) int {
+	lands := 0
+	var sink func(i, j int) int
+	sink = func(i, j int) int {
+		if i < 0 || j < 0 || i == len(grid) || j == len(grid[i]) || grid[i][j] == '0' {
+			return 0
+		}
+		grid[i][j] = '0'
+		sink(i-1, j)
+		sink(i+1, j)
+		sink(i, j-1)
+		sink(i, j+1)
+		return 1
+	}
+	for i := 0; i < len(grid); i++ {
+		for j := 0; j < len(grid[i]); j++ {
+			if grid[i][j] == '1' {
+				lands += sink(i, j)
+			}
+		}
+	}
+	return lands
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
 
 func main() {
 
